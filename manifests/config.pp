84cf23770aab102d60dd68ebd0559344fd718d25
rivethead42 <rivethead42@gmail.com>
2018-11-28 17:10:54 +0000

    Add module

--- manifests/config.pp
+++ manifests/config.pp
@@ -13,7 +13,7 @@ class nginx::config(
   String $docroot               = $::nginx::docroot,
 
 ) {
- file {'nginx.conf': 
+ file {'nginx_conf': 
 
  ensure => $ensure,
  path => "${config_dir}/nginx.conf",
@@ -22,5 +22,16 @@ class nginx::config(
  group => $group,
  content => template("${module_name}/conf.d/nginx.conf.erb"),
 }
+  file {$log_dir: 
+   ensure => directory,
+   recurse => true,
+}
+  file {$docroot:
 
+ ensure => directory,
+ recurse => true,
+ mode => $mode,
+ owner => $owner,
+ group => $group,
+ }
 }
